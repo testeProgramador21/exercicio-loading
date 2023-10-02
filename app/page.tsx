8a64d08444fc8f8047aeff70536248afd0be296c
testeProgramador21 <146689338+testeProgramador21@users.noreply.github.com>
2023-10-02 00:50:18 -0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+//LOADING
+
+const Page = () => {
+  return(
+    <div className="container mx-auto">
+      <div className="w-12 h-12 border-8">
+       
+      </div>
+    </div>
+  );
+}
+
+export default Page;

25f9d011594ea20174ee8545727af58ab4a87108
testeProgramador21 <146689338+testeProgramador21@users.noreply.github.com>
2023-10-02 00:55:49 -0300

    animação feita

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 const Page = () => {
   return(
     <div className="container mx-auto">
-      <div className="w-12 h-12 border-8">
+      <div className="w-12 h-12 border-8 border-blue-200 border-r-blue-600 border-t-blue-600 rounded-full animate-spin">
        
       </div>
     </div>
